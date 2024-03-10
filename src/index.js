9ab11c68560e70e7017bfde136f015ea89536630
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-03-10 16:34:31 +0100

    Initialize ApolloClient

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import GlobalStyles from "./styles";
 import Pages from "./pages";
+import GlobalStyles from "./styles";
 
 const root = createRoot(document.getElementById("root"));
 
+const client = new ApolloClient({
+  uri: "https://odyssey-lift-off-server.herokuapp.com/",
+  cache: new InMemoryCache(),
+});
+
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={client}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>
 );
