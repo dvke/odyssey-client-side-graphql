350e7c28e04b97d7ad92b3ccf8c2ad77ef22f937
Uche Obiekwe <obiekweuchenna@gmail.com>
2024-03-10 19:23:56 +0100

    Query with variables

--- src/pages/track.js
+++ src/pages/track.js
@@ -1,8 +1,49 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Layout } from "../components";
+import QueryResult from "../components/query-result";
+import TrackDetail from "../components/track-detail";
+
+export const GET_TRACK = gql`
+  query GetTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      description
+      numberOfViews
+      modules {
+        id
+        title
+        length
+        content
+        videoUrl
+      }
+    }
+  }
+`;
 
 const Track = () => {
-  return <Layout></Layout>;
+  const { trackId = "" } = useParams();
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Track;
